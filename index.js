220cc331093a067df25c9fee149e7313e8d78dff
maryam <32589085+maryamGoodarzi1994@users.noreply.github.com>
2025-09-28 13:58:44 +0330

    Update index.js

--- index.js
+++ index.js
@@ -2,19 +2,17 @@ import { calculatePrimes } from "./lib.mjs";
 import { Worker, isMainThread, parentPort } from "worker_threads";
 import os from "os";
 
+const end = 10000005;
 let start = 1;
 let workersCompleted = 0;
-const end = 10000005;
 
 if (isMainThread) {
   const numThreads = os.cpus().length;
   const range = Math.floor(end / numThreads);
-
   let total = 0;
 
   for (let i = 0; i < numThreads; i++) {
     const worker = new Worker("./index.js");
-
     worker.postMessage({
       start,
       end: i === numThreads - 1 ? end : start + range - 1,
